d6e7bef55eb179fe656a77f177728b9ad798c798
philcarlson <writephilcarlson@gmail.com>
2024-03-19 22:24:03 -0400

    Separates nav stacks

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -7,7 +7,9 @@ interface AuthContextType {
 }
 
 // Create context with a default value
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined,
+);
 
 // AuthProvider Props accepts children, so that we can place 'AuthProvider' at the top of the
 // component tree, wrapping the entire app.
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,26 +6,41 @@ import {SignupScreen} from '../onboarding/ui/Signup/SignupScreen';
 import {AuthProvider} from '../auth/AuthProvider';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useAuth} from '../auth/AuthProvider';
 
 const Stack = createNativeStackNavigator();
+
+const AuthStack = () => (
+  <Stack.Navigator
+    initialRouteName="Landing"
+    screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Landing" component={LandingScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Signup" component={SignupScreen} />
+  </Stack.Navigator>
+);
+
+const HomeStack = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Home" component={HomeScreen} />
+  </Stack.Navigator>
+);
 
 const App = () => {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Landing"
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Landing" component={LandingScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
+        <AppContent />
       </NavigationContainer>
     </AuthProvider>
   );
 };
 
+const AppContent = () => {
+  const {token} = useAuth();
+
+  return token ? <HomeStack /> : <AuthStack />;
+};
+
 export default App;
+
